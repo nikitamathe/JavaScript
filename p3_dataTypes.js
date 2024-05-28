900383c81a96ca415d41ac472e9a601373288519
nikita <nikitamathe12@gmail.com>
2024-05-28 20:44:17 +0530

    dataTypes

---
+++ p3_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+//alert("Hello")
+//alert(3+3) //we are using nodejs and not browser
+console.log(3 + 3)
+console.log("Nish")
+
+let name = "Nishita"
+let age = 18
+let isLoggedIn = false
+/* primitive data types */
+// number -> 2 to power 53
+// bigint
+// string -> " "
+// boolean -> true/false
+// null-> standalone (representation of empty value)
+// undefined -> not declared yet 
+// symbol -> unique
+
+
+
+// object
+
+console.log(typeof "Nishita");
+console.log(typeof age);
+console.log(typeof null); // object type
+console.log(typeof undefined); // undefined type
