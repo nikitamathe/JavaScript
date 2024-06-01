1e58d349fa4ac370592540fd46d35c887df5e9c7
nikita <nikitamathe12@gmail.com>
2024-06-01 12:57:15 +0530

    Methods of Date

---
+++ p9_datesInJs.js
@@ -0,0 +1,18 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.getDate());
+console.log(myDate.getDay);
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+console.log(myDate.getHours());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate); // Type of Date is Object
+console.log(myDate.getMilliseconds());
+console.log(myDate.getMinutes());
+console.log(myDate.getMonth());
+console.log(myDate.getSeconds());
+console.log(myDate.getTime());
+console.log(myDate.getTimezoneOffset());
+console.log(myDate.getUTCDate());
+console.log(myDate.getUTCDay());
