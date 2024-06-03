1af75412ff409028855a30738611796cd971e597
nikita <nikitamathe12@gmail.com>
2024-06-03 23:29:22 +0530

    object access and  adding functions to it

--- p12_objects.js
+++ p12_objects.js
@@ -4,24 +4,44 @@
 /* Object.create //constructor method // singleton */
 /* const JsUser ={} // empty object */
 
-
+const mySym = Symbol("key1")
 const JsUser = {
     name:"Nishita" ,  
     "full name" :"Nishita Singh",    // here key and value both can be given by as not like array 
-    mySym : "mykey1",
+   // mySym : "mykey1",
+   [mySym] : "mykey1", // we used [] for mySym because we want symbol as data type in line 24
     age: 29,
     location: "Jaipur",
-    email: "nishita@gmail.com",
+    email: "nishita@gmail.com",  // whatever key we declare it is by default considered as "key" (string) thats why  line 20 contains ["email"]
     isLoggedIn : false,
     lastLoginDays : ["Monday", "Saturday"]
 }
-console.log(JsUser.email) ;  // not a good practice   // full name is not accessible by this method 
-console.log(JsUser["email"]) ; // mail string ki tarah
-//console.log(JsUser.full name);
-console.log(JsUser["full name"]) ;
-console.log(JsUser.mySym) ; 
+  console.log(JsUser.email) ;  // not a good practice   // full name is not accessible by this method 
+    console.log(JsUser["email"]) ; // mail string ki tarah
+    //console.log(JsUser.full name);
+    console.log(JsUser["full name"]) ;
+    console.log(JsUser.mySym) ; 
+    console.log(typeof JsUser.mySym) ; //It should be Symbol type 
+    console.log(JsUser[mySym]);      
 
-const mySym = Symbol("key1")
+// object value access by dot notation 
+JsUser.email = "nishita2@gmail.com" // value change in object 
+console.log(JsUser.email) ; 
+//Object.freeze(JsUser) // If we want to lock values so that no changes will be made by anyone 
+JsUser.email = "nishita3@gmail.com" 
+console.log(JsUser.email) ; 
+
+// Function add
+JsUser.greeting = function(){
+    console.log("Hello! JS user");
+}
+console.log(JsUser.greeting);    //undefined
+console.log(JsUser.greeting()); //greeting is not a function because we freezed JsUser above
+
+
+
+
+
 
 
 
