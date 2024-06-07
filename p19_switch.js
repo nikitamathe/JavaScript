d73ee1fad3b150b185e3bf3c0aa898508987106e
nikita <nikitamathe12@gmail.com>
2024-06-07 23:31:16 +0530

    switch

---
+++ p19_switch.js
@@ -0,0 +1,55 @@
+
+const user = {
+    username: "nish",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function functOne(){
+//     let username = "nish"
+//     console.log(this.username);
+// }
+
+// functOne()
+
+// const functOne = function () {
+//     let username = "nish"
+//     console.log(this.username);
+// }
+
+const functOne =  () => {
+    let username = "nish"
+    console.log(this);
+}
+
+
+// functOne()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "nish"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
